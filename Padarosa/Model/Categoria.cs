3b544e5832839bc6dbaa2298ca237e67d3c6ea90
Habilidebob <80491104+habilidebob@users.noreply.github.com>
2025-06-30 15:16:01 -0300

    Initial commit
    Set up project structure and add initial files.

---
+++ Padarosa/Model/Categoria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padarosa.Model
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+
+        public int Cadastrar()
+        {
+
+        }
+        public DataTable Listar()
+        {
+
+        }
+        public int Excluir()
+        {
+
+        }
+        public int Modificar()
+        {
+
+        }
+    }
+}
---
+++ Padarosa/Model/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padarosa.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Email {  get; set; }
+        public string Senha { get; set; }
+
+        public DataTable Logar()
+        {
+
+        }
+        public int Cadastrar()
+        {
+
+        }
+        public DataTable Listar()
+        {
+
+        }
+
+        public int Remover()
+        {
+
+        }
+        public int Modificar()
+        {
+
+        }
+    }
+}
